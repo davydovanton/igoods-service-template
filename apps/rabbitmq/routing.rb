5bbb4f5de04e0fe4bcdcf9d28f134160bd08a778
Anton Davydov <antondavydov.o@gmail.com>
2020-07-27 22:08:37 +0300

    Add consumer class to the logic

--- apps/rabbitmq/routing.rb
+++ apps/rabbitmq/routing.rb
@@ -1,6 +1,9 @@
 module Rabbitmq
   class Routing
-    include Import[:logger]
+    include Import[
+      :logger,
+      employees_created_v1: 'rabbitmq.consumers.employees.created.v1'
+    ]
 
     def call(queue, event)
       event_type = [queue, event[:event_name], event[:event_version]]
@@ -8,6 +11,7 @@ module Rabbitmq
       case event_type
       in ['hello', 'test', 1]
         logger.info "Calling: #{queue}, #{event}"
+        employees_created_v1.call(event)
       else
         logger.info 'WRONG EVENT'
       end
