9b0b9228e1029b97930ccc391b9a21ffd7932089
Anton Davydov <antondavydov.o@gmail.com>
2020-07-28 00:19:40 +0300

    Update validation logic in producer

--- lib/core/bunny_publisher.rb
+++ lib/core/bunny_publisher.rb
@@ -15,8 +15,16 @@ module Core
 
     def call(queue_name:, event_name:, version:, data:)
       # validation
-      # yield SchemaRegistry.validate_event(event, 'billing.refund', version: 1).to_monad
+      # yield validate(event, 'billing.refund', version: 1).to_monad
       Success(exchange.publish(event.to_json, routing_key: queue_name))
+    end
+
+  private
+
+    def validate(event, event_name, version)
+      result = SchemaRegistry.validate_event(event, event_name, version: version)
+
+      result.success? ? Success(event) : Failure(result.failure)
     end
   end
 end
